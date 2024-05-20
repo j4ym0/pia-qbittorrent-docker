53c9196cca015f15a55afc7216f00bed80471b42
j4ym0 <jay_morrall@hotmail.com>
2024-05-20 22:36:32 +0100

    fix healthcheck across builds

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Check for tunnel 
-ifconfig | grep -cs 'tun0'  > /dev/null 2>&1
-if [ $? == 0 ]; then
+ifconfig | grep -cs 'tun0' > /dev/null 2>&1
+if [ $? -eq 0 ]; then
     # check qBittorrent is responding on web ui port
-    if [[ "$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:$WEBUI_PORT)" == "200" ]]; then
+    if [ "$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:$WEBUI_PORT)" -eq "200" ]; then
         exit 0
     fi
 fi
