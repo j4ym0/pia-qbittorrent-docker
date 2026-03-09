8f26865665ff42b194c8ac8492f053b234425290
j4ym0 <jay_morrall@hotmail.com>
2026-03-09 16:24:43 +0000

    fix healthcheck for wireguard

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check for tunnel 
-ifconfig | grep -cs 'tun0' > /dev/null 2>&1
+ifconfig | grep -cs 'tun0|pia' > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     # check qBittorrent is responding on web ui port
     HTTP_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:$WEBUI_PORT)
